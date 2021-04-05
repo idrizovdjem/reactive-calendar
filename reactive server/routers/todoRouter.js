2955d419c534a2bb7c80cbeb49219e0e4d8d785e
Dzhem Idrizov <idrizovdjem@gmail.com>
2021-04-05 19:25:06 +0300

    Add authentication middleware in todo router

--- reactive server/routers/todoRouter.js
+++ reactive server/routers/todoRouter.js
@@ -3,41 +3,35 @@ const authService = require('../services/authService.js');
 const todoService = require('../services/todoService.js');
 const router = express.Router();
 
-router.post('/create', async (request, response) => {
+router.use(async (request, response, next) => {
     const { authToken } = request.body;
     const userIdResponse = await authService.authenticateUser(authToken);
     if (!userIdResponse.successfull) {
         response.json({ response: userIdResponse });
-    } else {
-        const userId = userIdResponse.data.userId;
-        const { title, description, date, labelText } = request.body;
-        const todoResponse = await todoService.create(userId, title, description, date, labelText);
-        response.json({ response: todoResponse });
     }
+
+    const userId = userIdResponse.data.userId;
+    request.body.userId = userId;
+
+    next();
+});
+
+router.post('/create', async (request, response) => {
+    const { userId, title, description, date, labelText } = request.body;
+    const todoResponse = await todoService.create(userId, title, description, date, labelText);
+    response.json({ response: todoResponse });
 });
 
 router.post('/daily', async (request, response) => {
-    const { authToken, date } = request.body;
-    const userIdResponse = await authService.authenticateUser(authToken);
-    if (!userIdResponse.successfull) {
-        response.json({ response: userIdResponse });
-    } else {
-        const userId = userIdResponse.data.userId;
-        const todosResponse = await todoService.getForDate(userId, date);
-        response .json({ response: todosResponse });
-    }
+    const { userId, date } = request.body;
+    const todosResponse = await todoService.getForDate(userId, date);
+    response.json({ response: todosResponse });
 });
 
 router.post('/getForDateRange', async (request, response) => {
-    const { authToken, startDate, endDate } = request.body;
-    const userIdResponse = await authService.authenticateUser(authToken);
-    if (!userIdResponse.successfull) {
-        response.json({ response: userIdResponse });
-    } else {
-        const userId = userIdResponse.data.userId;
-        const todosResponse = await todoService.getForRange(userId, startDate, endDate);
-        response .json({ response: todosResponse });
-    }
+    const { userId, startDate, endDate } = request.body;
+    const todosResponse = await todoService.getForRange(userId, startDate, endDate);
+    response.json({ response: todosResponse });
 });
 
 module.exports = router;
