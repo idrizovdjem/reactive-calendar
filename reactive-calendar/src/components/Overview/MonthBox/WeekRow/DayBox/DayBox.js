70f133f7b27b9e6c6457b77924268e702fde260e
Dzhem Idrizov <idrizovdjem@gmail.com>
2021-04-11 13:28:36 +0300

    Add popover to overview day boxes

--- reactive-calendar/src/components/Overview/MonthBox/WeekRow/DayBox/DayBox.js
+++ reactive-calendar/src/components/Overview/MonthBox/WeekRow/DayBox/DayBox.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import classes from './DayBox.module.css';
 
+import calendarService from '../../../../../services/calendarService.js';
+
 class DayBox extends Component {
+    state = {
+        showPopup: false
+    }
+
+    hoverHandler = () => {
+        this.setState({ showPopup: true });
+    }
+
+    hoverLeaveHandler = () => {
+        this.setState({ showPopup: false });
+    }
+
     redirect = () => {
         const date = this.props.date;
         this.props.redirect(this.props.history, `/Todo/${date}`);
@@ -9,9 +23,25 @@ class DayBox extends Component {
 
     render() {
         const moodColor = this.props.moodColor;
+        let popup = null;
+        if (this.state.showPopup) {
+            const stringDate = calendarService.convertFromNumber(this.props.date);
+
+            popup = (
+                <div className={classes.Popover}>{stringDate}</div>
+            );
+        }
+
         return (
-            <div onClick={this.redirect} style={{ backgroundColor: moodColor }} className={classes.DayBox}>
+            <div
+                onMouseOver={this.hoverHandler}
+                onMouseLeave={this.hoverLeaveHandler}
+                onClick={this.redirect}
+                style={{ backgroundColor: moodColor }}
+                className={classes.DayBox}>
+                {popup}
             </div>
+
         );
     }
 }
